bda112d9b7c7bce8abb95853fc51353e7600e97b
areeb <areeb.malik@cooperativecomputing.com>
2023-01-24 13:24:07 +0500

    PUT request done

--- src/pages/EditProduct.js
+++ src/pages/EditProduct.js
@@ -5,77 +5,98 @@ export default function EditProduct() {
 
     // URLs
     const putUrl = 'http://localhost:8000/read-articles/';
-
-    // http://localhost:8000/update-article
-
-
-    // use state for displaying all the results
-    const [fullData, setFullData] = useState([]);
+    const getURL = 'http://localhost:8000/read-articles/';
 
     // get the URL param for id
     const urlID = new URLSearchParams(window.location.search).get('id');
+
+
+    const [formData, setFormData] = useState({
+        "author": "",
+        "title": "",
+        "description": "",
+        "url": "",
+        "urlToImage": "",
+        "content": ""
+    });
+
     useEffect(() => {
-        const singleData = async () => {
-            Axios.get(putUrl + urlID)
-                .then(function (result) {
-                    setFullData(result.data)
-                });
-        };
-        singleData();
-    }, [])
+        Axios.get(getURL + urlID)
+            .then(response => {
+                setFormData(response.data);
+            });
+    }, []);
 
-    const newJsonData = {
-        author: fullData.author,
-        title: fullData.title,
-        description: fullData.description,
-        url: fullData.url,
-        urlToImage: fullData.urlToImage,
-        publishedAt: fullData.publishedAt,
-        content: fullData.content,
-    };
-
-    // function to handle update
-    const handleUpdate = async () => {
-        const update = async () => {
-            Axios.put(putUrl + urlID, newJsonData);
-        }
-        update();
+    function handleSubmit(event) {
+        event.preventDefault();
+        Axios.put(putUrl + urlID, formData);
     }
 
     return (
         <div className='items'>
             <p className='display-1'>Edit Products</p>
-            <form onClick={handleUpdate} method='POST' className='content w-75'>
+            <form onSubmit={handleSubmit} className='content w-75'>
                 <div className="mb-3">
                     <label className="form-label">Author</label>
-                    <input className="form-control" type="text" value={fullData.author} onChange={e => setFullData(e.target.value)} />
+                    <input
+                        type="text"
+                        value={formData.author}
+                        className="form-control"
+                        onChange={event => setFormData({ ...formData, author: event.target.value })}
+                    />
                 </div>
 
                 <div className="mb-3">
                     <label className="form-label">Title</label>
-                    <input className="form-control" type="text" value={fullData.title} onChange={e => setFullData(e.target.value)} />
+                    <input
+                        type="text"
+                        value={formData.title}
+                        className="form-control"
+                        onChange={event => setFormData({ ...formData, title: event.target.value })}
+                    />
                 </div>
 
                 <div className="mb-3">
                     <label className="form-label">Description</label>
-                    <input className="form-control" type="text" value={fullData.description} onChange={e => setFullData(e.target.value)} />
+                    <input
+                        type="text"
+                        value={formData.description}
+                        className="form-control"
+                        onChange={event => setFormData({ ...formData, description: event.target.value })}
+                    />
                 </div>
 
                 <div className="mb-3">
                     <label className="form-label">URL</label>
-                    <input className="form-control" type="url" value={fullData.url} onChange={e => setFullData(e.target.value)} />
+                    <input
+                        type="text"
+                        value={formData.url}
+                        className="form-control"
+                        onChange={event => setFormData({ ...formData, url: event.target.value })}
+                    />
                 </div>
 
                 <div className="mb-3">
                     <label className="form-label">Image</label>
-                    <input className="form-control" type="url" value={fullData.urlToImage} onChange={e => setFullData(e.target.value)} />
+                    <input
+                        type="text"
+                        value={formData.urlToImage}
+                        className="form-control"
+                        onChange={event => setFormData({ ...formData, urlToImage: event.target.value })}
+                    />
                 </div>
 
                 <div className="mb-3">
                     <label className="form-label">Content</label>
-                    <input className="form-control" type="text" value={fullData.content} onChange={e => setFullData(e.target.value)} />
+                    <input
+                        type="text"
+                        value={formData.content}
+                        className="form-control"
+                        onChange={event => setFormData({ ...formData, content: event.target.value })}
+                    />
                 </div>
-                <button type="submit" className="btn btn-outline-success mt-4 btn-lg">Update Product</button>
+
+                <button type="submit" className="btn btn-outline-success mt-4 btn-lg">Update</button>
             </form>
         </div>
     )
