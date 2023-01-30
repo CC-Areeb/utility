8d797bfb1201ae98c5e94290880484d9e099ae11
areeb <areeb.malik@cooperativecomputing.com>
2023-01-30 14:44:55 +0500

    Working on category

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -96,6 +96,66 @@ export default function Categories() {
 import React from 'react'
 
 export default function Categories() {
+    // URLs
+    const apiURL = 'http://localhost:8000/category';
+    const searchUrl = "http://localhost:8000/category?q=";
+
+    // Basic info for pagination
+    const [pageNumber, setPageNumber] = useState(0);
+    const dataPerPage = 3;
+    const pagesVisited = pageNumber * dataPerPage;
+
+    // Get request
+    const [loading, setLoading] = useState(false);
+    const [state, setState] = useState([]);
+    useEffect(() => {
+        const dataFetch = async () => {
+            setLoading(true);
+            Axios.get(apiURL).then(function (response) {
+                setLoading(false);
+                setState(response.data.map((e) => { return e }));
+            });
+        };
+        dataFetch();
+    }, []);
+
+    // Sorting data after get request
+    const [isToggled, setisToggled] = useState(false);
+    function toggleClick() {
+        setisToggled(!isToggled);
+        const sortedDataDescending = state.sort((a, b) => -1);
+        setState(sortedDataDescending)
+    }
+
+    // displaying data
+    const displayData = state
+        .slice(pagesVisited, pagesVisited + dataPerPage)
+        .map(state => {
+            return <tr key={state.url}>
+                <td className='border'>{state.title}</td>
+                <td className='border'>{state.product_number}</td>
+                <td className='border'>{state.time_of_adding}</td>
+                <td className='border'>
+                    <NavLink to={`/category/edit?id=${state.id}`}>
+                        <button type="button" className="btn btn-outline-primary mx-2">
+                            Edit
+                            <i className="bi bi-pencil-square px-1" id='edit'></i>
+                        </button>
+                    </NavLink>
+                </td>
+            </tr>
+        });
+
+    // Search filter
+    const [search, setSearch] = useState("");
+    const handleSearchChange = e => {
+        setSearch(e.target.value);
+        Axios.get(searchUrl + e.target.value)
+            .then((data) => {
+                setState(data.data.map((e) => { return e }));
+            });
+    }
+
     return (
         <div>Categories</div>
 >>>>>>> Add existing project files to Git
